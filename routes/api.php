275339ee7a5a54b30c3701b26ffc786e6447647c
Rodrivas2000 <riverez50@gmail.com>
2023-11-30 20:10:38 -0600

    Practica sobre pruebas unitarias

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\respuestaController;
 use App\Http\Controllers\PaisController;
 use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\AutorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,6 +33,8 @@ Route::post("/nuevo",[PaisController::class,"crearPais"]);
 Route::get("/obtener",[PaisController::class,"obtener"]);
 
 Route::post('/login',[AuthController::class,'login']);
+
+Route::get('/autores',[AutorController::class,'obtenerAutores']);
 
 Route::post('/registrar',[AuthController::class,'register']);
 
